8f6cf1e970f11d81274f5f969ae913e60404aa30
LaCrak 27 <ivancrak123@gmail.com>
2023-07-01 16:10:53 +0200

    downlaods

--- pages/download/index.vue
+++ pages/download/index.vue
@@ -9,23 +9,8 @@
             </section>
             <section class="text-center lg:px-[15%] px-[5%]">
                 <p class="text-center uppercase font-medium tracking-wider mb-10 text-gray-400 lg:pt-20 pt-14">
-                    Latest (Zip File)
+                    We are currently migrating downloads, check again later or join the discord.
                 </p>
-                <a href="https://drive.google.com/uc?export=download&id=1gMschxUHQVXZsgz6SCTFM6YXr6abwD38">
-                    <button
-                        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                        Download
-                    </button>
-                </a>
-                <p class="text-center uppercase font-medium tracking-wider mb-10 text-gray-400 lg:pt-20 pt-14">
-                    Latest (Installer)
-                </p>
-                <a href="https://drive.google.com/uc?export=download&id=1gMschxUHQVXZsgz6SCTFM6YXr6abwD38">
-                    <button
-                        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                        Download
-                    </button>
-                </a>
             </section>
         </main>
         <Footer />
