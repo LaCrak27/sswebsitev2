b85188c79284a6b73ba7cf8b575c4c336afa2547
LaCrak 27 <ivancrak123@gmail.com>
2023-07-15 20:16:14 +0200

    fixed weird ass duplication

--- app.vue
+++ app.vue
@@ -1,13 +1,13 @@
 <template>
-  <head>
+  <Head>
     <link rel="icon" href="https://cdn.discordapp.com/attachments/1066709125184102449/1108122330745544714/StarStrikeLogo.png">
-    <meta content="Star Strike" property="og:title" />
-    <meta content="Star strike is an upcoming indie space game, which will contain a competitive multiplayer mode, as well as a single player story." property="og:description" />
-    <meta content="https://www.starstrikegame.com" property="og:url" />
-    <meta content="https://cdn.discordapp.com/attachments/1066709125184102449/1108122330745544714/StarStrikeLogo.png"
+    <Meta content="Star Strike" property="og:title" />
+    <Meta content="Star strike is an upcoming indie space game, which will contain a competitive multiplayer mode, as well as a single player story." property="og:description" />
+    <Meta content="https://www.starstrikegame.com" property="og:url" />
+    <Meta content="https://cdn.discordapp.com/attachments/1066709125184102449/1108122330745544714/StarStrikeLogo.png"
       property="og:image" />
-    <meta content="#26e6fb" data-react-helmet="true" name="theme-color" />
-  </head>
+    <Meta content="#26e6fb" data-react-helmet="true" name="theme-color" />
+  </Head>
 
   <div class="bg-[#0F121A] text-normal min-h-full">
     <NuxtPage />
