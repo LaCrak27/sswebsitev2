e1c15d14195bb247e9b779868d8da15e825ede2a
LaCrak 27 <ivancrak123@gmail.com>
2023-07-15 17:34:53 +0200

    some changes to the embed

--- app.vue
+++ app.vue
@@ -1,12 +1,11 @@
 <template>
   <head>
     <meta content="Star Strike" property="og:title" />
-    <meta content="Star strike is an upcoming indie space simulator, which will contain
-	a competitive multiplayer mode, as well as a single player story." property="og:description" />
+    <meta content="Star strike is an upcoming indie space game, which will contain a competitive multiplayer mode, as well as a single player story." property="og:description" />
     <meta content="https://www.starstrikegame.com" property="og:url" />
     <meta content="https://cdn.discordapp.com/attachments/1066709125184102449/1108122330745544714/StarStrikeLogo.png"
       property="og:image" />
-    <meta content="#000000" data-react-helmet="true" name="theme-color" />
+    <meta content="#26e6fb" data-react-helmet="true" name="theme-color" />
   </head>
 
   <div class="bg-[#0F121A] text-normal min-h-full">
